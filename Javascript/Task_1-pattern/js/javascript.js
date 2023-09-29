e7d83f520b20bafde1b432a28f0e6a3ad19decad
chordify <vignesh.venugopal@chordify.com>
2023-09-29 11:40:32 +0530

    javascript task 2 completed

--- Javascript/Task_1-pattern/js/javascript.js
+++ Javascript/Task_1-pattern/js/javascript.js
@@ -32,3 +32,9 @@ console.log(string);
 
 document.write("<h1>Hi all this is just basics of javascript task 1 and the pattern is printed below</h1>")
 
+//second day task
+function task2function() {
+    var input = document.getElementById("country").value;
+    document.getElementById("countryoutput").innerHTML =  input;
+  }
+
