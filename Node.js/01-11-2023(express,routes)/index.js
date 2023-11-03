bef446d5929592d5ab95b00a12ff56e92031faf9
chordify <vignesh.venugopal@chordify.com>
2023-11-03 09:08:48 +0530

    node

--- Node.js/01-11-2023(express,routes)/index.js
+++ Node.js/01-11-2023(express,routes)/index.js
@@ -1,36 +1,56 @@
-const express=require("express")
-const bodyparser=require("body-parser")
+const express = require("express")
+const bodyparser = require("body-parser")
+const mongoose = require("mongoose")
 
-app=express()
-port=3000
+
+
+
+
+app = express()
+port = 3000
+
+mongoose.connect('mongodb://127.0.0.1:27017/test').then(() => {
+    console.log("connected data base");
+})
+
+const User = mongoose.model("User", { name: String, age: Number, location: String })
 
 app.use(bodyparser.urlencoded())
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`server started in port ${port}`);
 })
 
-app.get("/",function(req,res){
+app.get("/", function (req, res) {
 
     res.send("hello world")
 })
 
-app.post("/post-method",function(req,res){
+app.post("/post-method", function (req, res) {
     res.send("this is post method")
 })
 
-app.put("/put-method",function(req,res){
+app.put("/put-method", function (req, res) {
     res.send("this is a put method")
 })
 
 
-app.patch("/patch-method",function(req,res){
+app.patch("/patch-method", function (req, res) {
     res.send("this is patch method")
 })
 
 
-app.post("/data-share",function(req,res){
+app.post("/data-share", function (req, res) {
 
-console.log("body data",req.body);
-    res.send(req.body)
+    const userData = new User()
+    userData.name = req.body.Name
+    userData.age = req.body.age
+    userData.location = req.body.location
+
+    userData.save().then((data) => {
+        console.log("data", data);
+    }).catch((err)=>{
+        console.log(err);
+    })
+
 })
